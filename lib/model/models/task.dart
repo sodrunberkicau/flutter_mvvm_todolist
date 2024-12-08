de29003c9db8d4fab194e2c8350e7232fcb25c84
Hannan <sodrunberkicau@gmail.com>
2024-12-09 00:34:36 +0800

    create flutter project and create task model

---
+++ lib/model/models/task.dart
@@ -0,0 +1,10 @@
+class Task {
+  const Task({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+  final int id;
+  final String title;
+  final bool isCompleted;
+}
